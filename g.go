4067738b9b3a9c76fc4fee2ac5377a2ee6eb6ec0
Andrew Jackson (Ajax) <snadrus@gmail.com>
2022-04-11 17:48:00 -0700

    Update g.go

--- g.go
+++ g.go
@@ -8,6 +8,17 @@ func Mapper[T, U any](feed []T, process func(T) U) []U {
    return res
 }
 
+// Reduce is the classic array reducer.
+// Ex: sum := g.Reduce([]int[1, 3, 5, 7}, 0, func(a, soFar int) int { return soFar + a })
+// Also useful for map filtering:
+//   type mt map[int]int
+//   m := map[int]int{2:4,6:8,10:12}
+//   filteredMap := g.Reduce(g.Keys(m), mt{}, func(k int, soFar mt) mt {
+//      if m[k] > 4 {
+//         soFar[k] = m[k]
+//      }
+//      return soFar
+//   }
 func Reduce[T, U any](feed []T, base U, process func(T, U) U) U {
    for _, v := range feed {
       base = process(v, base)
@@ -71,3 +82,76 @@ func Keys[T, U any](m map[T]U) []T {
    }
    return res
 }
+
+// TODO maptoys:
+func MapToys[K comparable, V any](m map[K]V, ...toys[K,V] MapToy) map[K]V {
+   for _, t := range toys {
+      m = t(m)
+   }
+}
+
+type MapToy[K comparable, V any] func(map[K]V) map[K]V)
+func MapToyClone[K comparable, V any](m map[K]V) map[K]V { x:=make(map[K]V, len(m)); for k, v := range m { x[k]=v }; return x }
+// MapToyFilter on keys
+func MapToyFilter[K comparable, V any](op Op[K]) func(m map[K]V, op Op[K]) map[K]V {
+   return func(map[K]V) map[K]V) {
+      x := make(map[K]V, len(m))
+      for k, v := range m {
+         if op[k] {
+            x[k] = v
+         }
+      }
+      return x
+   }
+}
+type Op[K comparable] func(k K) bool 
+
+// In Usage: g.MapToy(mapA, g.MapToyFilter(g.In(g.ToSet(mapB.Keys()))
+func In[V comparable](haystack Set[V]) Op {
+   return func(needle V) bool {
+      return haystack[needle]
+   }
+}
+
+func FlowOf[V any](v []V) chan V {
+   ch := make(chan V)
+   go func(){
+      defer close(ch)
+      for _, val  := range v {
+         v <- ch
+      }
+   }()
+   return ch
+}
+func Where[V comparable](ch chan V, f Op[V]) chan V {
+   ch2 := make(chan V)
+   go func() {
+      defer close(ch2)
+      for x  := range ch {
+         if f(x) {
+            ch2 <- x
+         }
+      }   
+   }()
+   return ch2
+}
+func Emit[V any](ch chan V) []V {
+   var res []V
+   for x := range ch {
+      res = append(res, x)
+   }
+   return res
+}
+func EmitMap[K comparable, V any](ch chan K, m map[K]V) map[K]V {
+   res := map[K]V{}
+   for x := range ch {
+      res[k] = m[k]
+   }
+   return res
+}
+
+//f := g.FlowOf(g.Keys(m))
+//f := g.Where(f, In(wantedKeys))
+// g.Emit(f) || g.EmitMap(f, srcMap)
+// Short version: g.EmitMap(g.Where(g.FlowOf(g.Keys(m)), g.In(wantedKeys)), m)
+          
